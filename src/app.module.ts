6b5849ea7ff09b1317d47ac3000a4339d71dd798
pablo10e36366 <pmortega@sudamericano.edu.ec>
2026-06-16 09:29:38 -0500

    feat: agregar modulo libros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LibrosModule } from './libros/libros.module';
 
 @Module({
-  imports: [],
+  imports: [LibrosModule],
   controllers: [AppController],
   providers: [AppService],
 })
