fa84423fd91f0a29e4b5ad69630a93484347a3fc
pablopablo007 <pablomorapina@gmail.com>
2026-06-16 09:24:49 -0500

    feat: generar modulo prestamo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrestamoModule } from './prestamo/prestamo.module';
 
 @Module({
-  imports: [],
+  imports: [PrestamoModule],
   controllers: [AppController],
   providers: [AppService],
 })
